76f0a814b66975e18359ec0e2c632176ec878f39
Ricardo Presilla <presilla.r@opendevpro.com>
2022-09-22 12:00:43 -0300

    Add class and files to config

---
+++ src/main/java/com/alfaCentauri/camel/CamelMockTestingApplication.java
@@ -0,0 +1,13 @@
+package com.alfaCentauri.camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelMockTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelMockTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alfaCentauri/camel/CamelMockTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfaCentauri.camel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMockTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
